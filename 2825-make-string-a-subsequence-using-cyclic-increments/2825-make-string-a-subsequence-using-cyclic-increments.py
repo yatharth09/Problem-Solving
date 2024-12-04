7ebb644ca223ebe840ecdf190a28cc394d95c98f
Yatharth Thapa <67639092+yatharth09@users.noreply.github.com>
2024-12-04 13:06:28 +0530

    Time: 131 ms (12.5%), Space: 17.6 MB (31.28%) - LeetHub

---
+++ 2825-make-string-a-subsequence-using-cyclic-increments/2825-make-string-a-subsequence-using-cyclic-increments.py
@@ -0,0 +1,13 @@
+class Solution:
+    def canMakeSubsequence(self, str1: str, str2: str) -> bool:
+        i = 0
+        j = 0
+        flag = True
+        while i < len(str1):
+            if j < len(str2) and (str1[i] == str2[j] or ((ord(str1[i]) + 1 == ord(str2[j]) or (str1[i] == 'z' and str2[j] == 'a')) and flag)):
+                if ord(str1[i]) == ord(str2[j]) + 1:
+                    flag = False
+                j += 1
+            i += 1
+        
+        return j == len(str2)
