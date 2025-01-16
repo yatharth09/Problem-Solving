c22b88df2841be913f8b46fc8224893d9d50b672
Yatharth Thapa <67639092+yatharth09@users.noreply.github.com>
2025-01-16 12:11:00 +0530

    Time: 1685 ms (13.76%), Space: 18.2 MB (62.73%) - LeetHub

--- 1930-unique-length-3-palindromic-subsequences/1930-unique-length-3-palindromic-subsequences.py
+++ 1930-unique-length-3-palindromic-subsequences/1930-unique-length-3-palindromic-subsequences.py
@@ -1,8 +1,6 @@
 class Solution:
     def countPalindromicSubsequence(self, s: str) -> int:
-        m = 26
-        n = 2 
-        alpha = [[-1 for _ in range(n)] for _ in range(m)]
+      
         ans = 0
         
         for i in range(26):
@@ -10,6 +8,7 @@ class Solution:
             left = -1
             right = -1
             char = chr(ord('a') + i)
+          
 
             for j in range(len(s)):
                 if s[j] == char and left != -1:
@@ -19,14 +18,13 @@ class Solution:
                     left = j
                 
                 
-            
             if left == -1 or right == -1:
-                break
+                continue
 
             mySet = set()
             for k in range(left + 1, right):
                 mySet.add(s[k])
-            
+            print(mySet)
 
             ans += len(mySet)
 
